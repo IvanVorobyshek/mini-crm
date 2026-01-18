b00eb94d37f2dc9dcd8a33ec5552a5cdcb652fcf
Ivan Voronin <ivanvorobyshek@gmail.com>
2026-01-18 12:38:57 +0200

    #mini-crm: Step 3. Eloquent Models have been added

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Mini-CRM
 Realization steps
 
 1. Project Initialization
- - Laravel 12, PHP 8.4, PostgreSQL, MailPit, Redis, xDebug, Laravel debugbar
+ - Laravel 12, PHP 8.4, PostgreSQL, MailPit, Redis, xDebug, Laravel debugbar, spatie
+
+2. Database migrations
+
+3. Eloquent Models (User, Customer, Ticket)
---
+++ app/Models/Ticket.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['customer_id', 'subject', 'description', 'status', 'manager_responded_at'];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'manager_responded_at' => 'datetime',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+        ];
+    }
+}
